0742c5e16c50213f3b15fdd7269113be3d639c30
pavan_08 <pavanchhatpar@gmail.com>
2017-10-28 00:54:19 +0530

    multiple screenshots with corner coordinate specifications

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 const Pageres = require('pageres');
- 
-const pageres = new Pageres({delay: 15})
-    .src('localhost:9000', ['1024x768'], {crop: true, filename: "<%= date %> - <%= time %>"})
+
+var count = 0;
+
+function takeSS(n, resolve) {
+    const pageres = new Pageres({delay: 5})
+    .src('localhost:9000?north=19.06601179781123&east=72.9323959350586&south=19.03761657578407&west=72.8737735748291&zoom=15', ['1024x768'], {crop: true, filename: "<%= date %> - <%= time %>"})
     .dest("images")
     .run()
-    .then(() => console.log('done'));
+    .then(() => {
+        count++;
+        n--;
+        if(n == 0) {
+            resolve();
+        } else {
+            takeSS(n, resolve);
+        }
+    });
+}
+
+new Promise((resolve, reject) => {
+    takeSS(2, resolve);    
+}).then(()=>console.log("Taken " + count + " screenshots."))
