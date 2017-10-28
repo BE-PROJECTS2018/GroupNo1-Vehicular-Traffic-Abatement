a2858e1e5d61695354691e0965dd94356d24abdc
pavan_08 <pavanchhatpar@gmail.com>
2017-10-28 10:40:00 +0530

    destination folder change

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var count = 0;
 function takeSS(n, resolve) {
     const pageres = new Pageres({delay: 5})
     .src('localhost:9000?north=19.06601179781123&east=72.9323959350586&south=19.03761657578407&west=72.8737735748291&zoom=15', ['1024x768'], {crop: true, filename: "<%= date %> - <%= time %>"})
-    .dest("images")
+    .dest("images/north=19.06601179781123&east=72.9323959350586&south=19.03761657578407&west=72.8737735748291")
     .run()
     .then(() => {
         count++;
