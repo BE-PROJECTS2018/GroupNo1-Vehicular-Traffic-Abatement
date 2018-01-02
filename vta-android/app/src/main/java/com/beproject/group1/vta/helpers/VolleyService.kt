1be966fbce6d85b701e7f59a690ac922f89d1208
pavan-08 <pavanchhatpar@gmail.com>
2018-01-02 21:08:09 +0530

    publishing service layer through "NOW"

--- vta-android/app/src/main/java/com/beproject/group1/vta/helpers/VolleyService.kt
+++ vta-android/app/src/main/java/com/beproject/group1/vta/helpers/VolleyService.kt
@@ -11,7 +11,7 @@ import org.json.JSONObject
 
 class VolleyService : ServiceInterface {
     val TAG = VolleyService::class.java.simpleName
-    val basePath = "http://192.168.1.111:3000/api/"
+    val basePath = "https://vtaservicelayer-hgkdnjachx.now.sh/api/"
 
     override fun post(path: String, params: JSONObject, completionHandler: (response: JSONObject?) -> Unit) {
         val jsonObjReq = object : JsonObjectRequest(Method.POST, basePath + path, params,
