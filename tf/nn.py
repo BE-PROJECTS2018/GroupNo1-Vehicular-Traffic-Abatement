6d69b1ac0253d4334b77f2d57bba68e407b3eb01
pavan-08 <pavanchhatpar@gmail.com>
2018-02-09 12:25:53 +0530

    utils fix

--- tf/nn.py
+++ tf/nn.py
@@ -1,5 +1,21 @@
 from utils import load_data
+import tensorflow as tf
 
 [training_set, test_set] = load_data('data')
 
 num_inputs = 5
+num_classes = 4
+
+n_hidden1 = 3
+n_hidden2 = 3
+
+learning_rate = 0.1
+
+# Specify that all features have real-value data
+feature_columns = [tf.feature_column.numeric_column("x", shape=[num_input])]
+
+dnn = tf.estimator.DNNClassifier(feature_columns=feature_columns,
+                                hidden_units=[n_hidden1, n_hidden2],
+                                n_classes=num_classes,
+                                optimizer=tf.train.AdagradOptimizer(learning_rate=learning_rate),
+                                model_dir='meta/nn_model')
--- tf/utils.py
+++ tf/utils.py
@@ -8,9 +8,17 @@ def load_data(dir):
         with open(path) as json_data:
             d = json.load(json_data)
         data.extend(d)
-    map(lambda t: t.pop('month'), data)
-    map(lambda t: t.pop('weekofyear'), data)
     shuffle(data)
-    total = len(data)
+    training_set = {'x': [], 'y': []}
+    test_set = {'x': [], 'y': []}
+    y = list(map(lambda t: [t.pop('t')], data))
+    x = list(map(lambda t: [t.pop('lat'), t.pop('lng'), t.pop('weekday'), t.pop('hour'), t.pop('min'), ], data))
+    total = len(x)
     n_train = (int)(total*0.8)
-    return [data[:n_train], data[n_train:]]
+    for i in range(n_train):
+        training_set['x'].append(x[i])
+        training_set['y'].append(y[i])
+    for i in range(n_train,total):
+        test_set['x'].append(x[i])
+        test_set['y'].append(y[i])
+    return [training_set, test_set]
