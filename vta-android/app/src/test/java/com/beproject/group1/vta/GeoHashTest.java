7fc802df710ddb810c3ec27100f80841486a43ba
pavan-08 <pavanchhatpar@gmail.com>
2018-03-15 02:46:02 +0530

    fix: shifting geohash test to test folder

--- vta-android/app/src/test/java/com/beproject/group1/vta/GeoHashTest.java
+++ vta-android/app/src/test/java/com/beproject/group1/vta/GeoHashTest.java
@@ -1,16 +1,13 @@
 package com.beproject.group1.vta;
 
-import android.support.test.runner.AndroidJUnit4;
-
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import static junit.framework.Assert.assertEquals;
 import ch.hsr.geohash.GeoHash;
 
 /**
  * Created by Sumeet on 14-03-2018.
  */
-@RunWith(AndroidJUnit4.class)
+
 public class GeoHashTest {
 
     private Double lat = 19.032801;
